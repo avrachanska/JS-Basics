c41f5271f2b00d5d810d6ceb0b19ffdc7885c118
avrachanska <avrachanska@mail.bg>
2016-06-27 14:36:42 +0300

    Create exBooks.js

---
+++ exBooks.js
@@ -0,0 +1,11 @@
+var harry_potter = new Object();
+harry_potter.pages = 350;
+harry_potter.author = "J.K. Rowling";
+
+// A custom constructor for book
+function Book (pages, author) {
+    this.pages = pages;
+    this.author = author;
+}
+
+var the_hobbit = new Book (320, "J.R.R. Tolkien");
