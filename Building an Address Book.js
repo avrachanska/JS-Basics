a6b9e4f648e9ab70d0631dc3b95da07dc99ba499
avrachanska <avrachanska@mail.bg>
2016-06-27 15:09:00 +0300

    Create Building an Address Book.js

---
+++ Building an Address Book.js
@@ -0,0 +1,31 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+//to check - printPerson(contacts[0]);
+//to check - printPerson(contacts[1]);
+
+function list() {
+    var contactsLength = contacts.length;
+    for (i = 0; i < contactsLength; i++) {
+        printPerson(contacts[i]);
+    }
+};
+
+list();
