68b5724fac4549b19d63f9129dca65eccbc57ab4
avrachanska <avrachanska@mail.bg>
2016-06-30 17:05:36 +0300

    Update Building an Address Book.js

--- Building an Address Book.js
+++ Building an Address Book.js
@@ -1,3 +1,6 @@
+//to check - printPerson(contacts[0]);
+//to check - printPerson(contacts[1]);
+
 var bob = {
     firstName: "Bob",
     lastName: "Jones",
@@ -18,14 +21,50 @@ function printPerson(person) {
     console.log(person.firstName + " " + person.lastName);
 }
 
-//to check - printPerson(contacts[0]);
-//to check - printPerson(contacts[1]);
-
 function list() {
-    var contactsLength = contacts.length;
-    for (i = 0; i < contactsLength; i++) {
-        printPerson(contacts[i]);
-    }
-};
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
 
+/*Create a search function
+then call it passing "Jones"*/
+/*function search(lastName){
+    var contactsLength = contacts.length;
+    for( i = 0; i < contactsLength; i++){
+        if(contacts[i].lastName === lastName){
+            printPerson(contacts[i]);
+        }
+    }
+}*/
+function search(lastName) {
+    contacts.forEach(function(contact) {
+        if (contact.lastName === lastName) {
+            printPerson(contact);
+        }
+    });
+}
+search("Jones");
+
+function add(firstName, lastName, email, phoneNumber){
+ var friend = {};
+    friend.firstName = firstName;
+    friend.lastName = lastName;
+    friend.phoneNumber = phoneNumber;
+    friend.email = email;
+    contacts[contacts.length] = friend;
+}
+
+/*function add(firstName, lastName, email, phoneNumber){
+	object = {
+	firstName: firstName,
+	lastName: lastName, 
+	email: email, 
+	phoneNumber: phoneNumber
+	};
+contacts[contacts.length] = object;
+}*/
+
+add("John", "Alden", "johnalden@email.com", "(0887) 123 456)");
 list();
